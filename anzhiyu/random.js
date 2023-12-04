4a6dc80eee9b3a42caddc045b2a1efcfe5241441
aYu <ayuisokay@qq.com>
2023-12-04 20:45:55 +0800

    Site updated: 2023-12-04 20:45:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/11/26/hello-world/"];function toRandomPost(){
+var posts=["2023/12/04/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
