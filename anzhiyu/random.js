7d1a9d90f148e109eaa5a8f5503da9865813c4fa
aYu <ayuisokay@qq.com>
2023-12-10 22:25:42 +0800

    Site updated: 2023-12-10 22:25:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/10/使用hexo搭建博客并部署到云服务器/","2023/12/04/hello-world/"];function toRandomPost(){
+var posts=["2023/12/10/JUC之线程池/","2023/12/04/hello-world/","2023/12/10/使用hexo搭建博客并部署到云服务器/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
