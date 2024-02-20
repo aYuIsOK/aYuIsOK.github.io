8b64373a0caca341c331de082f2e91357945ff3e
aYu <ayuisokay@qq.com>
2024-02-20 20:12:53 +0800

    Site updated: 2024-02-20 20:12:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/13/JVM之内存模型/","2023/12/10/JUC之线程池/","2023/12/13/JVM之垃圾回收/","2023/12/11/JVM之内存结构/","2023/12/13/JVM之类加载/","2023/12/04/hello-world/","2023/12/10/使用hexo搭建博客并部署到云服务器/"];function toRandomPost(){
+var posts=["2023/12/10/JUC之线程池/","2023/12/13/JVM之内存模型/","2023/12/04/hello-world/","2023/12/11/JVM之内存结构/","2023/12/10/使用hexo搭建博客并部署到云服务器/","2023/12/13/JVM之垃圾回收/","2023/12/13/JVM之类加载/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
